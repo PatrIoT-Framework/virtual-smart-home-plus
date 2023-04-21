0d3f69355d355d7c0d2241463df87c1792db2ba1
Marek Stastny <mstastny@redhat.com>
2023-04-21 14:51:54 +0200

    Create DTOs for Fireplace and Door

---
+++ src/main/java/io/patriotframework/virtualsmarthomeplus/DTOs/DoorDTO.java
@@ -0,0 +1,7 @@
+package io.patriotframework.virtualsmarthomeplus.DTOs;
+
+public class DoorDTO {
+    private String label;
+    private String status;
+    private Boolean enabled;
+}
---
+++ src/main/java/io/patriotframework/virtualsmarthomeplus/DTOs/FireplaceDTO.java
@@ -0,0 +1,7 @@
+package io.patriotframework.virtualsmarthomeplus.DTOs;
+
+public class FireplaceDTO {
+    private String label;
+    private String status;
+    private Boolean enabled;
+}
