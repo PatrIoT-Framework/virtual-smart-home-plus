1ba7d4f8f45f4649479a865345cdaea26a505edf
Marek Stastny <mstastny@redhat.com>
2023-04-25 18:50:43 +0200

    add ModelMaper bean

--- src/main/java/io/patriotframework/virtualsmarthomeplus/AppConfig.java
+++ src/main/java/io/patriotframework/virtualsmarthomeplus/AppConfig.java
@@ -4,6 +4,7 @@ import io.patriotframework.virtualsmarthomeplus.house.House;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.modelmapper.ModelMapper;
 
 @Configuration
 public class AppConfig {
@@ -11,5 +12,9 @@ public class AppConfig {
     @Bean
     public House houseProducer() {
         return new House();
+    }
+    @Bean
+    public ModelMapper modelMapper() {
+        return new ModelMapper();
     }
 }
